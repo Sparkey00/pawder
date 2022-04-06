271088b67877e9aca401e2b23bb445bb6666a0f5
moiseev <moiseev@rozetka.com.ua>
2022-04-06 16:25:37 +0300

    divised back

--- pawder_client/src/main.js
+++ pawder_client/src/main.js
@@ -2,5 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).use(store).use(router).mount('#app')
+
+axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;
+// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+createApp(App)
+  .use(Quasar, quasarUserOptions)
+  .use(store)
+  .use(router)
+  .use(VueAxios, axios)
+  .mount('#app')
